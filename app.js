ad0fb4ebc166124c3d923df465a1d9495a8fd2d7
Joseph <joe@josephmccarron.com>
2014-05-19 23:49:34 -0700

    added settings, cleaned up variables

--- app.js
+++ app.js
@@ -4,41 +4,54 @@
     events: {
       'app.activated':'minRead'
     },
-
     minRead: function() {
-        var wordsPerMinute = 180; // how fast most people can read on a monitor according to [Wikipedia](http://en.wikipedia.org/wiki/Words_per_minute#Reading_and_comprehension)
-        var comment = this.comment().text(); // the text of the current comment being entered
-        var CommentArray = comment.split(' '); // substring array of each word in the comment
-        var CommentArraySize = CommentArray.length; // size of the array
-        var test = CommentArraySize / wordsPerMinute; // how many minutes to read floating point integer
-        var minReadRounded = Math.round(test); // how many minutes to read rounded up to nearest integer
-        var plural;
-        var minutes;
-        if (minReadRounded < 1) {
-            minutes = 'less than a';
-        } else if (minReadRounded == 1) {
-            minutes = minReadRounded;
+        
+        var wordsPerMinute = this.setting('endUserSpeed'), // how fast most people can read on a monitor according to [Wikipedia](http://en.wikipedia.org/wiki/Words_per_minute#Reading_and_comprehension)
+            currentComment = this.comment().text(), // the text of the current comment being entered
+            words = currentComment.split(' '), // substring array of each word in the comment
+            length = words.length, // size of the array
+            time = length / wordsPerMinute, // how many minutes to read floating point integer
+            minutes = Math.round(time), // how many minutes to read rounded up to nearest integer
+            plural,
+            string;
+        if (minutes < 1) {
+            string = 'less than a';
+        } else if (minutes == 1) {
+            string = minutes;
         } else {
             plural = true;
-            minutes = minReadRounded;
+            string = minutes;
         }
         this.switchTo('moreThanMinute', {
-            minReadRounded: minutes,
+            minReadRounded: string,
             plural: plural
         });
-
-        var ticket = this.ticket();
-        var ticketComments = ticket.comments();
-        var ticketCommentsMap = _.map(ticketComments, function(comment){ return comment.value(); }); // array of substrings where each substring is an entire comment on the ticket
-        var ticketCommentsMapJoined = ticketCommentsMap.join(); // turn ticketCommentsMap into an entire string of all comments together
-        var ticketCommentsMapJoinedSplit = ticketCommentsMapJoined.split(' '); // turn ticketCommentsMapJoined into array of substrings, all words
-        var ticketCommentsMapJoinedSplitSize = ticketCommentsMapJoinedSplit.length; // number of substrings/words in ticketCommentsMapJoinedSplit
-        var test2 = ticketCommentsMapJoinedSplitSize / this.setting('wordsPerMinutedefault'); // how many minutes to read floating point integer
-        var minReadRoundedAll = Math.round(test2); // how many minutes to read rounded up to nearest integer
+        var plural2,
+            ticket = this.ticket(),
+            ticketComments = ticket.comments(),
+            ticketCommentsMap = _.map(ticketComments, function(comment){ return comment.value(); }), // array of substrings where each substring is an entire comment on the ticket
+            ticketCommentsMapJoined = ticketCommentsMap.join(), // turn ticketCommentsMap into an entire string of all comments together
+            ticketCommentsMapJoinedSplit = ticketCommentsMapJoined.split(' '), // turn ticketCommentsMapJoined into array of substrings, all words
+            ticketCommentsMapJoinedSplitSize = ticketCommentsMapJoinedSplit.length, // number of substrings/words in ticketCommentsMapJoinedSplit
+            test2 = ticketCommentsMapJoinedSplitSize / this.setting('agentSpeedDefault'), // how many minutes to read floating point integer
+            minReadRoundedAll = Math.round(test2); // how many minutes to read rounded up to nearest integer
+        if (minReadRoundedAll < 1) {
+            string2 = 'less than a';
+        } else if (minReadRoundedAll == 1) {
+            string2 = minReadRoundedAll;
+        } else {
+            plural2 = true;
+            string2 = minReadRoundedAll;
+        }
         this.switchTo('moreThanMinute', {
-            minReadRoundedAll: minReadRoundedAll,
+            minReadRounded: string2,
+            plural: plural
+        });
+        this.switchTo('moreThanMinute', {
             plural: plural,
-            minReadRounded: minutes
+            minReadRounded: string,
+            minReadRoundedAll: string2,
+            plural2: plural2
         });
     }
 };
